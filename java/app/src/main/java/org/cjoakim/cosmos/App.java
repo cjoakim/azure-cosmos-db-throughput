fc14a0cd17ef8bc3edb4238f5ff9f50ffacc0a56
Chris Joakim <christopher.joakim@gmail.com>
2023-11-03 16:18:06 -0400

    parse csv into objects

--- java/app/src/main/java/org/cjoakim/cosmos/App.java
+++ java/app/src/main/java/org/cjoakim/cosmos/App.java
@@ -1,11 +1,86 @@
 package org.cjoakim.cosmos;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cjoakim.cosmos.model.BaseballBatter;
+import org.cjoakim.cosmos.util.FileUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+    public static final String BASEBALL_BATTERS_CSV_FILE = "../../data/seanhahman-baseballdatabank-2023.1/core/Batting.csv";
+    private static Logger logger = LogManager.getLogger(App.class);
+
+    /**
+     * Entry point to the program.  It is driven by command-line args.
+     */
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            logger.warn("cli arg: " + i + " --> " + args[i]);
+        }
+        String function = args[0];
+
+        switch(function) {
+            case "load_cosmos_baseball_batters":
+                loadCosmos(args);
+                break;
+            default:
+                logger.error("undefined command-line function: " + function);
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    private static void loadCosmos(String[] args) {
+        String db = args[1];
+        String container = args[2];
+        String pk = args[3];
+        String pct = args[4];
+        String team = args[5];
+        List<BaseballBatter> batters = readBaseballBatters();
+
+        if (batters.size() < 1) {
+            logger.error("Batters list is empty");
+            return;
+        }
+    }
+
+    private static List<BaseballBatter> readBaseballBatters() {
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<BaseballBatter> batters = new ArrayList<BaseballBatter>();
+        try {
+            FileUtil fu = new FileUtil();
+            List<String> lines = fu.readLines(BASEBALL_BATTERS_CSV_FILE);
+            logger.warn("input file read; lines: " + lines.size());
+            String[] headerFields = null;
+
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+                if (i == 0) {
+                    headerFields = line.split("[,]", 0);
+                }
+                else {
+                    BaseballBatter bb = new BaseballBatter(headerFields, line);
+                    if (bb.isValid()) {
+                        batters.add(bb);
+                        if (i < 10) {
+                            logger.warn("csv line: " + line);
+                            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(bb));
+                        }
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            logger.fatal("unable to read input file " + BASEBALL_BATTERS_CSV_FILE);
+        }
+        logger.warn("batters read: " + batters.size());
+        return batters;
     }
 }
---
+++ java/app/src/main/java/org/cjoakim/cosmos/model/BaseballBatter.java
@@ -0,0 +1,139 @@
+package org.cjoakim.cosmos.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@Data
+public class BaseballBatter {
+
+    private static Logger logger = LogManager.getLogger(BaseballBatter.class);
+
+    // Instance variables - from the CSV file
+    String id;
+    String pk;
+    String playerID;
+    int    year;
+    String stint;
+    String teamID;
+    String leagueID;
+    int    games = -1;
+    int    atBats = -1;
+    int    runs = -1;
+    int    hits = -1;
+    int    doubles = -1;
+    int    triples = -1;
+    int    homeRuns = -1;
+    int    rbi = -1;
+    int    stolenBases = -1;
+    int    caughtStealing = -1;
+    int    baseOnBalls = -1;
+    int    strikeouts = -1;
+    int    intentionalBB = -1;
+    int    hitByPitch = -1;
+    boolean exception;
+
+    public BaseballBatter(String[] fields, String csvLine) {
+        String[] values = csvLine.split("[,]", 0);
+        if (fields.length == values.length) {
+            for (int i = 0; i < values.length; i++) {
+                String value = values[i].strip();
+                try {
+                    switch (i) {
+                        case 0:
+                            setId(UUID.randomUUID().toString());
+                            setPlayerID(value);
+                            setPk(value);
+                            break;
+                        case 1:
+                            setYear(Integer.parseInt(value));
+                            break;
+                        case 2:
+                            setStint(value);
+                            break;
+                        case 3:
+                            setTeamID(value);
+                            break;
+                        case 4:
+                            setLeagueID(value);
+                            break;
+                        case 5:
+                            setGames(Integer.parseInt(value));
+                            break;
+                        case 6:
+                            setAtBats(Integer.parseInt(value));
+                            break;
+                        case 7:
+                            setRuns(Integer.parseInt(value));
+                            break;
+                        case 8:
+                            setHits(Integer.parseInt(value));
+                            break;
+                        case 9:
+                            setDoubles(Integer.parseInt(value));
+                            break;
+                        case 10:
+                            setTriples(Integer.parseInt(value));
+                            break;
+                        case 11:
+                            setHomeRuns(Integer.parseInt(value));
+                            break;
+                        case 12:
+                            setRbi(Integer.parseInt(value));
+                            break;
+                        case 13:
+                            setStolenBases(Integer.parseInt(value));
+                            break;
+                        case 14:
+                            try {
+                                setCaughtStealing(Integer.parseInt(value));
+                            } catch (NumberFormatException e) {
+                                setCaughtStealing(0);
+                            }
+                            break;
+                        case 15:
+                            try {
+                                setBaseOnBalls(Integer.parseInt(value));
+                            } catch (NumberFormatException e) {
+                                setBaseOnBalls(0);
+                            }
+                            break;
+                        case 16:
+                            try {
+                                setStrikeouts(Integer.parseInt(value));
+                            } catch (NumberFormatException e) {
+                                setStrikeouts(0);
+                            }
+                            break;
+                        case 17:
+                            try {
+                                setIntentionalBB(Integer.parseInt(value));
+                            }
+                            catch (NumberFormatException e) {
+                                setIntentionalBB(0);
+                            }
+                            break;
+                        case 18:
+                            try {
+                                setHitByPitch(Integer.parseInt(value));
+                            } catch (NumberFormatException e) {
+                                setHitByPitch(0);
+                            }
+                            break;
+                    }
+                } catch (Exception e) {
+                    setException(true);
+                    logger.error("error on csv line index " + i + " value: <" + value + ">");
+                }
+            }
+        }
+    }
+
+    public boolean isValid() {
+        return games > 0;
+    }
+}
