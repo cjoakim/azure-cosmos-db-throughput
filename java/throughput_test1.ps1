1d7bed68befc443450ab2044553db45ab753eda2
Chris Joakim <christopher.joakim@gmail.com>
2023-11-06 17:33:42 -0500

    throughput_test1.ps1

--- java/throughput_test1.ps1
+++ java/throughput_test1.ps1
@@ -4,13 +4,13 @@ gradle clean
 gradle build
 
 echo 'deleting logfile ...'
-del tmp\throughput.txt
+del tmp\throughput_test1.txt
 
 echo 'local ...'
-gradle load_cosmos_baseball_batters_local_throughput  > tmp\throughput.txt
+gradle load_cosmos_baseball_batters_local_throughput  > tmp\throughput_test1.txt
 
 echo 'global ...'
-gradle load_cosmos_baseball_batters_global_throughput >> tmp\throughput.txt
+gradle load_cosmos_baseball_batters_global_throughput >> tmp\throughput_test1.txt
 
 echo 'done'
 
